9dc9ddeb73400ece3a342c2e42635d305db7a0a7
andkymon <andkymon@gmail.com>
2025-01-18 15:10:10 +0800

    Hide weather info on page load

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@
 import css from "./style.css";
 import { format } from "date-fns";
 
-const input = document.querySelector("input");
-const button = document.querySelector("button");
-
+//PREPARE THE DATA
 async function getWeatherInfoObject(location) {
     const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=2GGFS36ASKSN8ZDNPTTML7AZW&contentType=json`);
     const weatherInfoObject = await response.json();
@@ -49,7 +47,14 @@ function extractCurrentWeatherInformation(weatherInfoObject) {
     console.log("Wind: " + windSpeed + "km/h");
 }
 
+//EVENT LISTENERS
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+
+const weatherInfo = document.querySelector("#weather-info-wrapper");
+
 button.addEventListener("click", async () => {
+    weatherInfo.style.display = "block";
     const location = input.value;
     const weatherInfoObject = await getWeatherInfoObject(location);
     extractCurrentWeatherInformation(weatherInfoObject);
@@ -57,8 +62,17 @@ button.addEventListener("click", async () => {
 
 input.addEventListener("keypress", async (event) => {
     if (event.key === "Enter") {
+        weatherInfo.style.display = "block";
+
         const location = input.value;
         const weatherInfoObject = await getWeatherInfoObject(location);
         extractCurrentWeatherInformation(weatherInfoObject);
     }
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    weatherInfo.style.display = "none";
+});
+
+//INITIALLY HIDE WEATHER INFO ON LOAD
+//TITLE AND INPUT CENTERED
