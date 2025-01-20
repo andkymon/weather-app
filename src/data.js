32dffbbc69007b31b677cddffc93e1849eab4773
andkymon <andkymon@gmail.com>
2025-01-20 10:58:10 +0800

    Apply prettier

--- src/data.js
+++ src/data.js
@@ -1,58 +1,60 @@
 // Fetch the data and parse it as an object
 async function getWeatherInformationRaw(locationInput) {
-    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${locationInput}?unitGroup=metric&key=2GGFS36ASKSN8ZDNPTTML7AZW&contentType=json`);
-    const weatherInfoObject = await response.json();
-    console.log(weatherInfoObject);
-    return weatherInfoObject;
+  const response = await fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${locationInput}?unitGroup=metric&key=2GGFS36ASKSN8ZDNPTTML7AZW&contentType=json`,
+  );
+  const weatherInfoObject = await response.json();
+  console.log(weatherInfoObject);
+  return weatherInfoObject;
 }
 
-// Only extract necessary information from the 
+// Only extract necessary information from the
 function filterWeatherInformation(weatherInfoObject) {
-    // Location (City, Country)
-    let location;
-    const addressArray = weatherInfoObject.resolvedAddress.split(", ");
-    const city = addressArray[0];
-    const country = addressArray[addressArray.length - 1];
+  // Location (City, Country)
+  let location;
+  const addressArray = weatherInfoObject.resolvedAddress.split(", ");
+  const city = addressArray[0];
+  const country = addressArray[addressArray.length - 1];
 
-    if (city === country) {
-        location = country;
-        console.log(location);
-    } else {
-        location = city + ", " + country;
-        console.log(location);
-    }
+  if (city === country) {
+    location = country;
+    console.log(location);
+  } else {
+    location = city + ", " + country;
+    console.log(location);
+  }
 
-    //Weather condition icon
-    const conditionIcon = weatherInfoObject.currentConditions.icon;
+  //Weather condition icon
+  const conditionIcon = weatherInfoObject.currentConditions.icon;
 
-    //Temperature (Celcius)
-    const temperature = weatherInfoObject.currentConditions.temp;
+  //Temperature (Celcius)
+  const temperature = weatherInfoObject.currentConditions.temp;
 
-    //Weather condition
-    const condition = weatherInfoObject.currentConditions.conditions;
+  //Weather condition
+  const condition = weatherInfoObject.currentConditions.conditions;
 
-    //Precipitation
-    const precipitation = weatherInfoObject.currentConditions.precipprob;
+  //Precipitation
+  const precipitation = weatherInfoObject.currentConditions.precipprob;
 
-    //Humidity
-    const humidity = weatherInfoObject.currentConditions.humidity;
+  //Humidity
+  const humidity = weatherInfoObject.currentConditions.humidity;
 
-    //Wind Speed
-    const windSpeed = weatherInfoObject.currentConditions.windspeed;
+  //Wind Speed
+  const windSpeed = weatherInfoObject.currentConditions.windspeed;
 
-    return {
-        location,
-        conditionIcon,
-        temperature,
-        condition,
-        precipitation,
-        humidity,
-        windSpeed
-    }
+  return {
+    location,
+    conditionIcon,
+    temperature,
+    condition,
+    precipitation,
+    humidity,
+    windSpeed,
+  };
 }
 
 export async function getWeatherInformationFiltered(locationInput) {
-    const raw = await getWeatherInformationRaw(locationInput);
-    const filtered = filterWeatherInformation(raw);
-    return filtered;
+  const raw = await getWeatherInformationRaw(locationInput);
+  const filtered = filterWeatherInformation(raw);
+  return filtered;
 }
